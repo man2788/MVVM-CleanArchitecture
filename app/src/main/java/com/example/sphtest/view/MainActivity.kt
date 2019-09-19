486ebba787694d157848d035c026c619b66372d7
ABDUL Naseer M <venn53@sg.uob>
2019-09-19 23:48:41 +0800

    - Add | Progress bar

--- app/src/main/java/com/example/sphtest/view/MainActivity.kt
+++ app/src/main/java/com/example/sphtest/view/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.sphtest.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.sphtest.R
 import com.example.sphtest.adapter.DataUsageListAdapter
 import com.example.sphtest.adapter.GridItemDecoration
-import com.example.sphtest.domain.Entity.DataUsageEntity
 import com.example.sphtest.extensions.hasNetwork
 import com.example.sphtest.extensions.showTaost
 import com.example.sphtest.viewmodel.DataUsageViewModel
@@ -17,16 +17,21 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : AppCompatActivity() {
 
     private val dataUsageViewModel: DataUsageViewModel by viewModel()
-    private var isLoading: Boolean? = false
-    var data = mutableListOf<DataUsageEntity>()
+    private var isLoading: Boolean= false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(this.hasNetwork()){
+        if(!this.hasNetwork()){
             this.showTaost(getString(R.string.internet_connection))
+            return
         }
         dataUsageViewModel.isLoadingLiveData.observe(this, Observer {
             isLoading = it
+            if(isLoading){
+                progressBar.visibility = View.VISIBLE
+            }else{
+                progressBar.visibility = View.GONE
+            }
         })
         prepareRecyclerView()
     }
@@ -34,12 +39,12 @@ class MainActivity : AppCompatActivity() {
     private fun prepareRecyclerView() {
         recyclerView_dataList.layoutManager = GridLayoutManager(this, 2)
         recyclerView_dataList.addItemDecoration(GridItemDecoration(10, 2))
-        val dataUsageAdapter = DataUsageListAdapter(data, this)
+        val dataUsageAdapter = DataUsageListAdapter()
         recyclerView_dataList.adapter = dataUsageAdapter
         recyclerView_dataList.setHasFixedSize(true)
-        dataUsageViewModel.datausageList.observe(this, Observer {
+        dataUsageViewModel.dataUsageListLiveData.observe(this, Observer {
             if(!it.isNullOrEmpty())
-            dataUsageAdapter.updateAdapter(it)
+                dataUsageAdapter.updateAdapter(it)
         })
     }
 }
